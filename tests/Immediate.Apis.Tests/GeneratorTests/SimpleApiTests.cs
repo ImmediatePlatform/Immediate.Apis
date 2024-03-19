20988d16c6bc203d18d8c620e7c3739aaccf032a
Stuart Turner <stuart@turner-isageek.com>
2024-03-19 15:12:27 -0500

    Add additional generator tests

--- tests/Immediate.Apis.Tests/GeneratorTests/SimpleApiTests.cs
+++ tests/Immediate.Apis.Tests/GeneratorTests/SimpleApiTests.cs
@@ -81,4 +81,58 @@ public sealed class SimpleApiTests
 		_ = await Verify(result)
 			.UseParameters(method);
 	}
+
+	[Theory]
+	[InlineData("Get")]
+	[InlineData("Post")]
+	[InlineData("Patch")]
+	[InlineData("Put")]
+	[InlineData("Delete")]
+	public async Task MapMultipleHandlersTest(string method)
+	{
+		var driver = GeneratorTestHelper.GetDriver(
+			$$"""
+			using System.Threading.Tasks;
+			using Immediate.Apis.Shared;
+			using Immediate.Handlers.Shared;
+			
+			namespace Dummy;
+			
+			[Handler]
+			[Map{{method}}("/test")]
+			public static class GetUsersQuery
+			{
+				public record Query;
+			
+				private static ValueTask<int> HandleAsync(
+					Query _,
+					CancellationToken token)
+				{
+					return 0;
+				}
+			}
+			
+			[Handler]
+			[Map{{method}}("/test")]
+			public static class GetUserQuery
+			{
+				public record Query(int Id);
+			
+				private static async ValueTask<int> HandleAsync(
+					Query _,
+					CancellationToken token)
+				{
+					return 0;
+				}
+			}
+			""");
+
+		var result = driver.GetRunResult();
+
+		Assert.Empty(result.Diagnostics);
+		_ = Assert.Single(result.GeneratedTrees);
+
+		_ = await Verify(result)
+			.UseParameters(method);
+	}
 }

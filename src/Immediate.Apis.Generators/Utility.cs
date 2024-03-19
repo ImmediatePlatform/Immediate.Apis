844853e84c0ac40e00bf42c74c1e3ce1c7c5c8e5
Stuart Turner <stuart@turner-isageek.com>
2024-03-19 11:16:05 -0500

    Improve coverage

--- src/Immediate.Apis.Generators/Utility.cs
+++ src/Immediate.Apis.Generators/Utility.cs
@@ -7,27 +7,6 @@ namespace Immediate.Apis.Generators;
 
 internal static class Utility
 {
-	public static bool ImplementsBaseClass(this INamedTypeSymbol typeSymbol, INamedTypeSymbol typeToCheck) =>
-		SymbolEqualityComparer.Default.Equals(typeSymbol, typeToCheck)
-		|| (typeSymbol.BaseType is not null
-			&& ImplementsBaseClass(typeSymbol.BaseType.OriginalDefinition, typeToCheck)
-		   );
-
-	public static ITypeSymbol? GetTaskReturnType(this IMethodSymbol method, INamedTypeSymbol taskSymbol) =>
-		SymbolEqualityComparer.Default.Equals(method.ReturnType.OriginalDefinition, taskSymbol)
-			? ((INamedTypeSymbol)method.ReturnType).TypeArguments.FirstOrDefault()
-			: null;
-
-	public static AttributeData? GetAttribute(this INamedTypeSymbol symbol, string attribute) =>
-		symbol
-			.GetAttributes()
-			.FirstOrDefault(a =>
-				a.AttributeClass?.ToString() == attribute
-			);
-
-	public static string? NullIf(this string value, string check) =>
-		value.Equals(check, StringComparison.Ordinal) ? null : value;
-
 	public static Template GetTemplate(string name)
 	{
 		using var stream = Assembly

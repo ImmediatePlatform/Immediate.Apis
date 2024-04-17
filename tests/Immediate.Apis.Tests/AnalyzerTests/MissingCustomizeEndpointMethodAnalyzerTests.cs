92bf73cd8a5e785c2d41e7561459df5d5bf5db51
Duke <40759437+dukesteen@users.noreply.github.com>
2024-04-17 16:50:19 +0200

    `CustomizeEndpoint` codefix (#39)
    ---------
    
    Co-authored-by: Duke <commits@dmail.icu>
    Co-authored-by: Stuart Turner <stuart@turner-isageek.com>

---
+++ tests/Immediate.Apis.Tests/AnalyzerTests/MissingCustomizeEndpointMethodAnalyzerTests.cs
@@ -0,0 +1,77 @@
+using System.Diagnostics.CodeAnalysis;
+using Immediate.Apis.Analyzers;
+
+namespace Immediate.Apis.Tests.AnalyzerTests;
+
+// NB: CS0234 is due to lack of `Microsoft.AspNetCore.Http.Abstractions` library
+// TODO: figure out how to reference `Microsoft.AspNetCore.App.Ref`
+
+[SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Test names")]
+public sealed class MissingCustomizeEndpointMethodAnalyzerTests
+{
+	[Theory]
+	[MemberData(nameof(Utility.Methods), MemberType = typeof(Utility))]
+	public async Task ValidDefinitionShouldRaiseHiddenDiagnostic(string method)
+	{
+		await AnalyzerTestHelpers.CreateAnalyzerTest<MissingCustomizeEndpointMethodAnalyzer>(
+			$$"""
+			using System.Threading;
+			using System.Threading.Tasks;
+			using Immediate.Apis.Shared;
+			using Immediate.Handlers.Shared;
+			using Microsoft.AspNetCore.Authorization;
+					
+			namespace Dummy;
+
+			[Handler]
+			[Map{{method}}("/test")]
+			public static class {|IAPI0006:GetUsersQuery|}
+			{
+				public record Query;
+
+				private static async ValueTask<int> Handle(
+					Query _,
+					CancellationToken token)
+				{
+					return 0;
+				}
+			}
+			"""
+		).RunAsync();
+	}
+
+	[Theory]
+	[MemberData(nameof(Utility.Methods), MemberType = typeof(Utility))]
+	public async Task ValidDefinition_WithExistingCustomizeEndpointMethod_ShouldNotRaiseHiddenDiagnostic(string method)
+	{
+		await AnalyzerTestHelpers.CreateAnalyzerTest<MissingCustomizeEndpointMethodAnalyzer>(
+			$$"""
+			using System.Threading;
+			using System.Threading.Tasks;
+			using Immediate.Apis.Shared;
+			using Immediate.Handlers.Shared;
+			using Microsoft.AspNetCore.Authorization;
+					
+			namespace Dummy;
+
+			[Handler]
+			[Map{{method}}("/test")]
+			public static class GetUsersQuery
+			{
+				internal static void CustomizeEndpoint(Microsoft.AspNetCore.Builder.IEndpointConventionBuilder endpoint)
+					=> endpoint
+						.WithDescription("");
+
+				public record Query;
+
+				private static async ValueTask<int> Handle(
+					Query _,
+					CancellationToken token)
+				{
+					return 0;
+				}
+			}
+			"""
+		).RunAsync();
+	}
+}
---
+++ tests/Immediate.Apis.Tests/CodeFixTests/MissingCustomizeEndpointMethodCodeFixTests.cs
@@ -0,0 +1,65 @@
+using System.Diagnostics.CodeAnalysis;
+using Immediate.Apis.Analyzers;
+using Immediate.Apis.CodeFixes;
+
+namespace Immediate.Apis.Tests.CodeFixTests;
+
+[SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Test names")]
+public sealed class MissingCustomizeEndpointMethodCodeFixTests
+{
+	[Theory]
+	[MemberData(nameof(Utility.Methods), MemberType = typeof(Utility))]
+	public async Task ValidDefinition_ShouldAddCustomizeEndpointMethod(string method)
+	{
+		await CodeFixTestHelper
+			.CreateCodeFixTest<MissingCustomizeEndpointMethodAnalyzer, MissingCustomizeEndpointMethodCodeFixProvider>(
+				$$"""
+				using System.Threading;
+				using System.Threading.Tasks;
+				using Immediate.Apis.Shared;
+				using Immediate.Handlers.Shared;
+
+				namespace Dummy;
+
+				[Handler]
+				[Map{{method}}("/test")]
+				public static class {|IAPI0006:GetUsersQuery|}
+				{
+					public record Query;
+				
+					private static async ValueTask<int> Handle(
+						Query _,
+						CancellationToken token)
+					{
+						return 0;
+					}
+				}
+				""",
+				$$"""
+				using System.Threading;
+				using System.Threading.Tasks;
+				using Immediate.Apis.Shared;
+				using Immediate.Handlers.Shared;
+				using Microsoft.AspNetCore.Builder;
+
+				namespace Dummy;
+
+				[Handler]
+				[Map{{method}}("/test")]
+				public static class GetUsersQuery
+				{
+					internal static void CustomizeEndpoint(IEndpointConventionBuilder endpoint) => {|CS0201:endpoint|};
+
+					public record Query;
+
+					private static async ValueTask<int> Handle(
+						Query _,
+						CancellationToken token)
+					{
+						return 0;
+					}
+				}
+				"""
+			).RunAsync();
+	}
+}

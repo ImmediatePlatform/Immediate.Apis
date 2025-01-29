029d2d7f48a58fb2963035ec436a9e755f6761e4
Stuart Turner <stuart@turner-isageek.com>
2025-01-29 10:15:03 -0600

    Fix bug in codefix adding `TransformResult` (#155)

--- tests/Immediate.Apis.Tests/CodeFixTests/MissingTransformResultMethodCodeFixTests.cs
+++ tests/Immediate.Apis.Tests/CodeFixTests/MissingTransformResultMethodCodeFixTests.cs
@@ -9,7 +9,7 @@ public sealed class MissingTransformResultMethodCodeFixTests
 {
 	[Test]
 	[MethodDataSource(typeof(Utility), nameof(Utility.Methods))]
-	public async Task ValidDefinition_ShouldAddTransformResultMethod(string method)
+	public async Task ValidDefinitionHandle_ShouldAddTransformResultMethod(string method)
 	{
 		await CodeFixTestHelper
 			.CreateCodeFixTest<MissingTransformResultMethodAnalyzer, MissingTransformResultMethodCodeFixProvider>(
@@ -59,6 +59,68 @@ public sealed class MissingTransformResultMethodCodeFixTests
 					public record Response;
 				
 					private static async ValueTask<IReadOnlyList<Response>> Handle(
+						Query _,
+						CancellationToken token)
+					{
+						return [];
+					}
+				}
+				"""
+			).RunAsync();
+	}
+
+	[Test]
+	[MethodDataSource(typeof(Utility), nameof(Utility.Methods))]
+	public async Task ValidDefinitionHandleAsync_ShouldAddTransformResultMethod(string method)
+	{
+		await CodeFixTestHelper
+			.CreateCodeFixTest<MissingTransformResultMethodAnalyzer, MissingTransformResultMethodCodeFixProvider>(
+				$$"""
+				using System.Threading;
+				using System.Threading.Tasks;
+				using Immediate.Apis.Shared;
+				using Immediate.Handlers.Shared;
+				using System.Collections.Generic;
+
+				namespace Dummy;
+
+				[Handler]
+				[Map{{method}}("/test")]
+				public static class {|IAPI0007:GetUsersQuery|}
+				{
+					public record Query;
+					public record Response;
+				
+					private static async ValueTask<IReadOnlyList<Response>> HandleAsync(
+						Query _,
+						CancellationToken token)
+					{
+						return [];
+					}
+				}
+				""",
+				$$"""
+				using System.Threading;
+				using System.Threading.Tasks;
+				using Immediate.Apis.Shared;
+				using Immediate.Handlers.Shared;
+				using System.Collections.Generic;
+
+				namespace Dummy;
+
+				[Handler]
+				[Map{{method}}("/test")]
+				public static class GetUsersQuery
+				{
+					internal static IReadOnlyList<Response> TransformResult(IReadOnlyList<Response> result)
+					{
+						return result;
+					}
+
+					public record Query;
+					public record Response;
+				
+					private static async ValueTask<IReadOnlyList<Response>> HandleAsync(
 						Query _,
 						CancellationToken token)
 					{

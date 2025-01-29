dae847e216e7e7b42524f13ec0b7206e69afaf3d
Stuart Turner <stuart@turner-isageek.com>
2025-01-29 10:53:06 -0600

    Support API when `CancellationToken` is not present (#159)

--- tests/Immediate.Apis.Tests/GeneratorTests/SimpleApiTests.cs
+++ tests/Immediate.Apis.Tests/GeneratorTests/SimpleApiTests.cs
@@ -220,4 +220,45 @@ public sealed class SimpleApiTests
 
 		_ = await Verify(result);
 	}
+
+	[Test]
+	[MethodDataSource(typeof(Utility), nameof(Utility.Methods))]
+	public async Task MapMethodMissingCancellationToken(string method)
+	{
+		var result = GeneratorTestHelper.RunGenerator(
+			$$"""
+			using System.Threading;
+			using System.Threading.Tasks;
+			using Immediate.Apis.Shared;
+			using Immediate.Handlers.Shared;
+			
+			namespace Dummy;
+
+			[Handler]
+			[Map{{method}}("/test")]
+			public static partial class GetUsersQuery
+			{
+				public record Query;
+
+				private static ValueTask<int> Handle(
+					Query _
+				)
+				{
+					return ValueTask.FromResult(0);
+				}
+			}
+			""");
+
+		Assert.Equal(
+			[
+				@"Immediate.Apis.Generators/Immediate.Apis.Generators.ImmediateApisGenerator/RouteBuilder.Dummy_GetUsersQuery.g.cs",
+				@"Immediate.Apis.Generators/Immediate.Apis.Generators.ImmediateApisGenerator/RoutesBuilder.g.cs",
+				@"Immediate.Handlers.Generators/Immediate.Handlers.Generators.ImmediateHandlers.ImmediateHandlersGenerator/IH.Dummy.GetUsersQuery.g.cs",
+				@"Immediate.Handlers.Generators/Immediate.Handlers.Generators.ImmediateHandlers.ImmediateHandlersGenerator/IH.ServiceCollectionExtensions.g.cs",
+			],
+			result.GeneratedTrees.Select(t => t.FilePath.Replace('\\', '/'))
+		);
+
+		_ = await Verify(result).UseParameters(method);
+	}
 }

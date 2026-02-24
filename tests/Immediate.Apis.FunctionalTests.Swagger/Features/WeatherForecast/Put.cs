3c3bcae837a7c332fb86532ad4269a81c2e22886
Stuart Turner <stuart@turner-isageek.com>
2026-02-24 06:06:48 -0600

    Unify endpoints for consistent dev work

--- tests/Immediate.Apis.FunctionalTests.Swagger/Features/WeatherForecast/Put.cs
+++ tests/Immediate.Apis.FunctionalTests.Swagger/Features/WeatherForecast/Put.cs
@@ -1,6 +1,7 @@
 using Immediate.Apis.Shared;
 using Immediate.Handlers.Shared;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Immediate.Apis.FunctionalTests.Swagger.Features.WeatherForecast;
 
@@ -11,12 +12,30 @@ public static partial class Put
 {
 	public sealed record Command
 	{
+		/// <summary>
+		/// The date of the temperature data
+		/// </summary>
+		[FromRoute]
 		public required DateOnly Date { get; init; }
+
+		/// <summary>
+		/// The temperature for the specified date, in Celsius
+		/// </summary>
 		public required int TemperatureC { get; init; }
+
+		/// <summary>
+		/// Additional information about the date's weather
+		/// </summary>
 		public required string? Summary { get; init; }
+
 		public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 	}
 
+	/// <summary>Save Temperature Data</summary>
+	/// <remarks>
+	/// Saves temperature data for a particular date.
+	/// </remarks>
+	/// <response code="200">Data saved successfully</response>
 	private static async ValueTask Handle(
 		[AsParameters]
 		Command _,

7e12df6cd81f3f5c36f5f44d693f6b674eccfe6e
Stuart Turner <stuart@turner-isageek.com>
2026-07-14 15:10:49 -0500

    Fix for Nullable parameters (#313)

--- tests/Immediate.Apis.Tests/GeneratorTests/SimpleApiTests.cs
+++ tests/Immediate.Apis.Tests/GeneratorTests/SimpleApiTests.cs
@@ -47,6 +47,49 @@ public sealed class SimpleApiTests
 
 	[Theory]
 	[MemberData(nameof(Methods), MemberType = typeof(Utility))]
+	public async Task MapMethodNullableParameterTest(string method)
+	{
+		var result = GeneratorTestHelper.RunGenerator(
+			$$"""
+			#nullable enable
+
+			using System.Threading;
+			using System.Threading.Tasks;
+			using Immediate.Apis.Shared;
+			using Immediate.Handlers.Shared;
+			
+			namespace Dummy;
+
+			[Handler]
+			[Map{{method}}("/test")]
+			public sealed partial class GetUsersQuery
+			{
+				public record Query;
+
+				private ValueTask<int> Handle(
+					Query? _,
+					CancellationToken token)
+				{
+					return ValueTask.FromResult(0);
+				}
+			}
+			""");
+
+		Assert.Equal(
+			[
+				@"Immediate.Apis.Generators/Immediate.Apis.Generators.ImmediateApisGenerator/IA.Dummy.GetUsersQuery.g.cs",
+				@"Immediate.Apis.Generators/Immediate.Apis.Generators.ImmediateApisGenerator/IA.MapEndpoints.g.cs",
+				@"Immediate.Handlers.Generators/Immediate.Handlers.Generators.ImmediateHandlersGenerator/IH.Dummy.GetUsersQuery.g.cs",
+				@"Immediate.Handlers.Generators/Immediate.Handlers.Generators.ImmediateHandlersGenerator/IH.ServiceCollectionExtensions.g.cs",
+			],
+			result.GeneratedTrees.Select(t => t.FilePath.Replace('\\', '/'))
+		);
+
+		_ = await VerifyIgnoreImmediateHandlers(result).UseParameters(method);
+	}
+
+	[Theory]
+	[MemberData(nameof(Methods), MemberType = typeof(Utility))]
 	public async Task MapMethodHandleAsyncTest(string method)
 	{
 		var result = GeneratorTestHelper.RunGenerator(

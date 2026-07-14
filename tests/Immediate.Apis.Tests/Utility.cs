a18d7f59cac2868688f59c1115c38efa2c711907
Stuart Turner <stuart@turner-isageek.com>
2026-07-14 14:55:01 -0500

    Update dependencies

--- tests/Immediate.Apis.Tests/Utility.cs
+++ tests/Immediate.Apis.Tests/Utility.cs
@@ -32,7 +32,7 @@ internal static partial class Utility
 		"net11.0",
 		new PackageIdentity(
 			"Microsoft.NETCore.App.Ref",
-			"11.0.0-preview.5.26302.115"
+			"11.0.0-preview.6.26359.118"
 		),
 		Path.Combine("ref", "net11.0")
 	);
